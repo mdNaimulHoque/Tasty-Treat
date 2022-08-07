c3f7b4cdf6e720d85ca643810319189b18ac01b6
mdNaimulHoque <naimulhoq89@gmail.com>
2022-08-07 12:23:26 +0600

    project is almost done

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,116 @@
+import React from "react";
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+import logo from "../../assets/images/res-logo.png";
+
+import "../../styles/footer.css";
+
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="3" md="4" sm="6">
+            <div className=" footer__logo text-start">
+              <img src={logo} alt="logo" />
+              <h5>Tasty Treat</h5>
+              <p>
+                Develop by: <br />
+                <small>
+                  Roll: 172645 Roll: 172646 Roll: 172647 Roll: 175123 Roll:
+                  175149 Roll: 175734 Roll: 175736
+                </small>
+              </p>
+            </div>
+          </Col>
+
+          <Col lg="3" md="4" sm="6">
+            <h5 className="footer__title">Delivery Time</h5>
+            <ListGroup className="deliver__time-list">
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span>Sunday - Thursday</span>
+                <p>10:00am - 11:00pm</p>
+              </ListGroupItem>
+
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span>Friday - Saturday</span>
+                <p>Off day</p>
+              </ListGroupItem>
+            </ListGroup>
+          </Col>
+
+          <Col lg="3" md="4" sm="6">
+            <h5 className="footer__title">Contact</h5>
+            <ListGroup className="deliver__time-list">
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <p>Location: Chawkbazar, Chittagong Sadar: 4203, Bangladesh</p>
+              </ListGroupItem>
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span>Phone: 01800-000000</span>
+              </ListGroupItem>
+
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span>Email: example@gmail.com</span>
+              </ListGroupItem>
+            </ListGroup>
+          </Col>
+
+          <Col lg="3" md="4" sm="6">
+            <h5 className="footer__title">Newsletter</h5>
+            <p>Subscribe our newsletter</p>
+            <div className="newsletter">
+              <input type="email" placeholder="Enter your email" />
+              <span>
+                <i class="ri-send-plane-line"></i>
+              </span>
+            </div>
+          </Col>
+        </Row>
+
+        <Row className="mt-5">
+          <Col lg="6" md="6">
+            <p className="copyright__text">
+              Copyright - 2022, website made by{" "}
+              <span className="footer__title">Group-E</span>. All Rights
+              Reserved.
+            </p>
+          </Col>
+          <Col lg="6" md="6">
+            <div className="social__links d-flex align-items-center gap-4 justify-content-end">
+              <p className="m-0">Follow: </p>
+              <span>
+                {" "}
+                <Link to="https://www.facebook.com/muhib160">
+                  <i class="ri-facebook-line"></i>
+                </Link>{" "}
+              </span>
+
+              <span>
+                <Link to="https://github.com/muhib160">
+                  <i class="ri-github-line"></i>
+                </Link>
+              </span>
+
+              <span>
+                {" "}
+                <Link to=" https://www.youtube.com/c/MuhibsTechDiary">
+                  <i class="ri-youtube-line"></i>
+                </Link>{" "}
+              </span>
+
+              <span>
+                {" "}
+                <Link to=" https://www.linkedin.com/in/muhib160/">
+                  <i class="ri-linkedin-line"></i>
+                </Link>{" "}
+              </span>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
